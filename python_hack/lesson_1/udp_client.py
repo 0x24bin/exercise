138a181139304b6fcfee42fdbe7054d10339000d
grt1st <jintianlu@qq.com>
2016-09-29 19:19:34 +0800

    Delete udp_client.py

--- python_hack/lesson_1/udp_client.py
+++
@@ -1,7 +0,0 @@
-import socket
-target_host="127.0.0.1"
-target_port=80
-client=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-client.sendto("hello,world",(target_host,target_post))
-data,addr=client.recvfrom(4096)
-print data
