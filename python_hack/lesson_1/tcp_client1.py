a5a96501b37140f24934a2c3c70bad91a4596c19
grt1st <jintianlu@qq.com>
2016-09-29 19:19:27 +0800

    Delete tcp_client1.py

--- python_hack/lesson_1/tcp_client1.py
+++
@@ -1,8 +0,0 @@
-import socket
-target_host="192.168.10.108"
-target_port=9999
-client=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-client.connect((target_host,target_port))
-client.send("ASD")
-response=client.recv(4096)
-print response
